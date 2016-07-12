ef6457d97fc1379f84b0f677f6bff7da71f4ad74
AngeLopez <angel@angelopez.ca>
2016-07-12 13:53:03 -0400

    Views with no styles

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
-  def index
 
+  def index
+    @listings = Listing.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'listings' => 'listings#index'
+  get 'listings/:id' => 'listings#show', as: 'listing'
 
 end
