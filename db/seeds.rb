2ac30734713a8ddd47b0ae69d9fe837bd1077ece
AngeLopez <angel@angelopez.ca>
2016-07-12 13:00:16 -0400

    Seeds data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.create(
+  item:'Floppy Hat',
+  brand:'Aldo',
+  quantity:1,
+  url:'https://images.unsplash.com/photo-1450297085576-21d98821f1fb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f32a6df98803b0d3d0dc076804a98a4c'
+)
+Listing.create(
+  item:'Record Player',
+  brand:'Audio Technica',
+  quantity:1,
+  url:'https://images.unsplash.com/photo-1468164016595-6108e4c60c8b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=ef8b263dde206bab40dd8006c68bf837'
+)
+Listing.create(
+  item:'Single Speed Bicycle',
+  brand:'Linus',
+  quantity:1,
+  url:'https://images.unsplash.com/photo-1465657617727-18fdde26533e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=957114de40e0b3838aa4ad262ac788e7'
+)
+Listing.create(
+  item:'Three-legged Woooden Coffee Tables',
+  brand:'IKEA',
+  quantity:2,
+  url:'https://images.unsplash.com/photo-1461418559055-6f020c5a91e7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=99f9e8eab5b9af3f559795023ca90684'
+)
+Listing.create(
+  item:'Fresh Blueberries',
+  brand:'Garden Fresh',
+  quantity:8,
+  url:'https://images.unsplash.com/photo-1460400408855-36abd76648b9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=1c9fc275d85ee05c6e64f09eff62b363'
+)
