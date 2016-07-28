ddee2a240a995076020f2c361ca422fc12b1cf49
AngeLopez <angel@angelopez.ca>
2016-07-28 18:01:19 -0400

    Posting feature for items, html firm made

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,4 +7,12 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    render text: "Posting an item. URL: #{params[:url]}, Item: #{params[:item]}, Brand: #{params[:brand]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'listings' => 'listings#index'
+
+  get 'listings/new' => 'listings#new'
+  post 'listings' => 'listings#create'
+
   get 'listings/:id' => 'listings#show', as: 'listing'
 
 end
