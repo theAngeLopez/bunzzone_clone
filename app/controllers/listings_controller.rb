ae7da337ced30e88eadcedba0e458fc397643094
AngeLopez <angel@angelopez.ca>
2016-08-04 17:43:07 -0400

    refactored new listing form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,10 +9,21 @@ class ListingsController < ApplicationController
   end
 
   def new
-
+    @listing = Lisitng.new(listing_params)
+    if @listing.same
+      redirect_to listings_url
+    else
+      render :new
+    end
   end
 
   def create
     render text: "Posting an item. URL: #{params[:url]}, Item: #{params[:item]}, Brand: #{params[:brand]}"
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:item, :brand, :quantity, :url)
+
   end
 end
